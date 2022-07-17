83e8076ba9db98e9031e56ade6e319c76a2565eb
vixx_ <vittorio.adesso@gmail.com>
2022-07-17 13:50:15 -0400

    fix: adding locale middleware

--- back/back/settings/shared.py
+++ back/back/settings/shared.py
@@ -119,6 +119,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "django.middleware.locale.LocaleMiddleware",
 ]
 
 ROOT_URLCONF = "back.urls"
